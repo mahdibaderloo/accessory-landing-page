c30ec600a4318b9e9883cc8bbe11e370b4c63db8
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-05-18 11:10:49 +0330

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,46 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppLayout from "./components/AppLayout";
+import Home from "./components/Home";
+import Cart from "./features/cart/Cart";
+import LoginPage from "./features/user/LoginPage";
+import Order from "./features/order/Order";
+import Products from "./features/products/Products";
+import Product from "./features/products/Product";
+
 function App() {
-  return <div></div>;
+  const router = createBrowserRouter([
+    {
+      element: <AppLayout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/cart",
+          element: <Cart />,
+        },
+        {
+          path: "/login",
+          element: <LoginPage />,
+        },
+        {
+          path: "/order",
+          element: <Order />,
+        },
+        {
+          path: "/Products",
+          element: <Products />,
+        },
+        {
+          path: "/Products/:productId",
+          element: <Product />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+function Username() {
+  return <div></div>;
+}
+
+export default Username;
