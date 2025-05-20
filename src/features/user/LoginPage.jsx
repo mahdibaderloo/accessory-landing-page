0f68d0b625010769a6a1d89c91ea451ad15a2916
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-05-20 10:07:54 +0330

    login template

--- src/features/user/LoginPage.jsx
+++ src/features/user/LoginPage.jsx
@@ -1,5 +1,33 @@
+import { Link } from "react-router-dom";
+
 function LoginPage() {
-  return <div></div>;
+  return (
+    <div className="flex">
+      <section className="w-full grow h-screen bg-zinc-100">
+        <form action="">
+          <img src="" alt="" />
+          <p>Sign in</p>
+          <div>
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" placeholder="Username..." />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" placeholder="Email..." />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" placeholder="Password..." />
+          </div>
+          <button>SIGN IN</button>
+          <p>
+            I don't have an account? <Link to="/signup">Sign up</Link>
+          </p>
+        </form>
+      </section>
+      <section className="hidden w-[65%] h-screen bg-zinc-800"></section>
+    </div>
+  );
 }
 
 export default LoginPage;
