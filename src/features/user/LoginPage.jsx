5a211f1ce11b861ab99990c583453d25b7c0d9ac
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-05-20 10:40:40 +0330

    login styles

--- src/features/user/LoginPage.jsx
+++ src/features/user/LoginPage.jsx
@@ -3,25 +3,52 @@ import { Link } from "react-router-dom";
 function LoginPage() {
   return (
     <div className="">
-      <section className="w-full h-screen bg-zinc-100">
-        <form action="">
-          <img src="" alt="" />
-          <p>Sign in</p>
-          <div>
-            <label htmlFor="username">Username</label>
-            <input type="text" id="username" placeholder="Username..." />
+      <section className="w-full h-screen p-2 bg-zinc-100 flex justify-center items-center">
+        <form action="" className="w-full flex flex-col gap-3">
+          <p className="font-bold text-5xl mb-5 mt-2 mx-auto text-zinc-800">
+            Sign in
+          </p>
+          <div className="flex flex-col">
+            <label className="text-sm mb-0.5 text-zinc-800" htmlFor="username">
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              placeholder="Username..."
+              className="bg-zinc-300 p-0.5 pl-2 outline-none"
+            />
           </div>
-          <div>
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" placeholder="Email..." />
+          <div className="flex flex-col">
+            <label className="text-sm mb-0.5 text-zinc-800" htmlFor="email">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              placeholder="Email..."
+              className="bg-zinc-300 p-0.5 pl-2 outline-none"
+            />
           </div>
-          <div>
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password" placeholder="Password..." />
+          <div className="flex flex-col">
+            <label className="text-sm mb-0.5 text-zinc-800" htmlFor="password">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Password..."
+              className="bg-zinc-300 p-0.5 pl-2 outline-none"
+            />
           </div>
-          <button>SIGN IN</button>
-          <p>
-            I don't have an account? <Link to="/signup">Sign up</Link>
+          <button className="mt-5 bg-zinc-800 w-20 mx-auto rounded-full text-zinc-100 p-1">
+            SIGN IN
+          </button>
+          <p className="mx-auto mt-2 text-sm text-zinc-500">
+            I don't have an account?{" "}
+            <Link className="text-zinc-900" to="/signup">
+              Sign up
+            </Link>
           </p>
         </form>
       </section>
