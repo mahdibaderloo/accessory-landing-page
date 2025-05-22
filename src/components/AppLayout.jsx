d463ae79816ebfe9b04915dd1371f85b8b261ed5
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-05-22 19:33:55 +0330

    header for mobile size

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Header from "./Header";
 
 function AppLayout() {
   return (
     <div className="font-barlow">
+      <Header />
       <Outlet />
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+
 function Header() {
-  return <div></div>;
+  return (
+    <header className="flex justify-between bg-zinc-800 pl-2 absolute top-0 left-0 right-0">
+      <img src="../../public/menu.svg" className="w-10" alt="menu" />
+      <Link to="/">
+        <img src="../../public/logo.svg" className="w-12" alt="logo" />
+      </Link>
+    </header>
+  );
 }
 
 export default Header;
