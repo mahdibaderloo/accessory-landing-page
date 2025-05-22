8bab2df048ca16e82b6bfef371a5c082e9978457
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-05-22 19:12:08 +0330

    edit loader styles

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,6 @@
 function Loader() {
   return (
-    <div className="w-[150px] h-screen relative mx-auto flex items-center">
+    <div className="w-[115px] h-screen relative mx-auto flex justify-center items-center">
       <div className="absolute w-[33px] h-[33px] rounded-full bg-zinc-800 left-[0px] animate-scale1"></div>
       <div className="absolute w-[33px] h-[33px] rounded-full bg-zinc-800 left-[0px] animate-translate"></div>
       <div className="absolute w-[33px] h-[33px] rounded-full bg-zinc-800 left-[40px] animate-translate"></div>
