7f9be6375e57959757a1507a86b3639e80441d38
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-05-25 19:13:08 +0330

    create store

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,19 @@
 import { Link } from "react-router-dom";
+import Sidebar from "./Sidebar";
 
 function Header() {
+  function handleOpenSidebar() {
+    console.log("click");
+  }
+
   return (
     <header className="flex justify-between bg-zinc-800 pl-2 absolute top-0 left-0 right-0">
-      <img src="../../public/menu.svg" className="w-10" alt="menu" />
+      <img
+        src="../../public/menu.svg"
+        className="w-10"
+        alt="menu"
+        onClick={handleOpenSidebar}
+      />
       <Link to="/">
         <img src="../../public/logo.svg" className="w-12" alt="logo" />
       </Link>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 
-function Sidebar() {
+function Sidebar({ left = "-120%" }) {
   return (
-    <ul className="w-full h-screen bg-zinc-800 absolute left-0 top-0 p-2 pl-4 flex flex-col gap-4 text-zinc-200">
+    <ul
+      className={`w-full h-screen bg-zinc-800 absolute left-[${left}] top-0 p-2 pl-4 flex flex-col gap-4 text-zinc-200 transition-all duration-300 ease-in-out`}
+    >
       <li className="flex justify-end mb-6">
         <img src="../../public/close.svg" alt="close" className="w-8" />
       </li>
