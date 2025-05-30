9b594803ed4e2133774195a6ae3f15d492bc34a4
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-05-30 13:02:49 +0330

    header responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,47 @@
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import { openSidebar } from "./sidebarSlice";
+import { closeSidebar, openSidebar } from "./sidebarSlice";
 
 function Header() {
   const dispatch = useDispatch();
 
   return (
-    <header className="flex justify-between pl-2 absolute top-0 left-0 right-0 desktop:max-w-[1536px] desktop:mx-auto">
+    <header className="flex justify-between items-center pl-2 absolute top-0 left-0 right-0 desktop:max-w-[1536px] desktop:mx-auto desktop:h-16">
       <img
         src="/menu.svg"
-        className="w-10"
+        className="w-10 laptop:hidden"
         alt="menu"
         onClick={() => dispatch(openSidebar(0))}
       />
+      <ul className="hidden laptop:flex gap-6 font-semibold text-zinc-800 pl-2 desktop:font-bold">
+        <li>
+          <Link to="/products" onClick={() => dispatch(closeSidebar())}>
+            Products
+          </Link>
+        </li>
+        <li>
+          <Link to="/Cart" onClick={() => dispatch(closeSidebar())}>
+            Shopping Cart
+          </Link>
+        </li>
+        <li>
+          <Link to="/login" onClick={() => dispatch(closeSidebar())}>
+            Sign in
+          </Link>
+        </li>
+        <li>
+          <Link to="/" onClick={() => dispatch(closeSidebar())}>
+            About us
+          </Link>
+        </li>
+        <li>
+          <Link to="/" onClick={() => dispatch(closeSidebar())}>
+            Contact us
+          </Link>
+        </li>
+      </ul>
       <Link to="/">
-        <img src="/logo.svg" className="w-12" alt="logo" />
+        <img src="/logo.svg" className="w-12 desktop:w-14" alt="logo" />
       </Link>
     </header>
   );
