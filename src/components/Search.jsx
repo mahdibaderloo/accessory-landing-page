b016891cf5bc17759fc66b29bd8cbe056236656f
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-06-02 13:45:24 +0330

    edit search box styles

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,13 @@
 function Search() {
   return (
-    <form className="w-[90%] tablet:w-[80%] laptop:w-[50%] bg-gradient-to-r from-zinc-100 via-zinc-300 to-zinc-200 mx-auto mt-16 h-12 flex gap-2 rounded-full p-2 shadow">
-      <img
-        src="./search.svg"
-        alt="search-icon"
-        className="w-5 tablet:w-7 cursor-pointer"
-      />
+    <form className="w-[90%] tablet:w-[80%] laptop:w-[50%] bg-gradient-to-r from-zinc-100 via-zinc-300 to-zinc-200 mx-auto mt-16 h-12 flex gap-2 rounded-full p-1 shadow">
+      <div className="bg-zinc-800 px-3.5 py-1 tablet:py-2 tablet:px-2.5 rounded-full flex justify-center items-center">
+        <img
+          src="./search.svg"
+          alt="search-icon"
+          className="w-4 tablet:w-6 cursor-pointer"
+        />
+      </div>
       <input
         type="text"
         id=""
