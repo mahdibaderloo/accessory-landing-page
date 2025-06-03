2bca9e4cc09516b276891ea3375dd73aa5040917
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-06-03 12:00:20 +0330

    navigate to order page

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,6 @@
 function Loader() {
   return (
-    <div className="absolute bg-zinc-300 w-full h-screen">
+    <div className="w-full h-screen">
       <div className="w-[115px] h-screen relative mx-auto flex justify-center items-center">
         <div className="absolute w-[33px] h-[33px] rounded-full bg-zinc-800 left-[0px] animate-scale1"></div>
         <div className="absolute w-[33px] h-[33px] rounded-full bg-zinc-800 left-[0px] animate-translate"></div>
--- src/features/pages/Cart.jsx
+++ src/features/pages/Cart.jsx
@@ -1,6 +1,9 @@
+import { useNavigate } from "react-router-dom";
 import CartItem from "../cart/CartItem";
 
 function Cart() {
+  const navigate = useNavigate();
+
   return (
     <div className="w-full flex flex-col">
       <ul className="h-[60%] rounded-lg mt-13 p-2 laptop:p-3 desktop:p-10 laptop:h-[65vh] laptop:overflow-y-scroll">
@@ -32,7 +35,10 @@ function Cart() {
           <p>$161</p>
         </div>
       </div>
-      <button className="bg-zinc-800 w-24 h-8 rounded-md text-zinc-100 mx-auto mb-4 mt-2 laptop:cursor-pointer laptop:w-40 laptop:h-12 laptop:rounded-xl laptop:text-2xl hover:bg-zinc-700 transition-all duration-200">
+      <button
+        className="bg-zinc-800 w-24 h-8 rounded-md text-zinc-100 mx-auto mb-4 mt-2 laptop:cursor-pointer laptop:w-40 laptop:h-12 laptop:rounded-xl laptop:text-2xl hover:bg-zinc-700 transition-all duration-200"
+        onClick={() => navigate("/order")}
+      >
         Checkout
       </button>
     </div>
