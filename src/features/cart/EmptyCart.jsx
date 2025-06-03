d8c3b768f04252363523805398cff98b4448f651
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-06-03 12:10:54 +0330

    empty cart

---
+++ src/features/cart/EmptyCart.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+function EmptyCart() {
+  return (
+    <div className="w-full h-screen mt-16 flex flex-col justify-center items-center gap-6">
+      <p className="text-4xl font-semibold">Cart is empty!</p>
+      <Link
+        to="/products"
+        className="bg-zinc-800 py-1 px-2 rounded-md text-zinc-100 hover:bg-zinc-600 transition-all duration-200"
+      >
+        Back to shop
+      </Link>
+    </div>
+  );
+}
+
+export default EmptyCart;
--- src/features/pages/Cart.jsx
+++ src/features/pages/Cart.jsx
@@ -1,46 +1,47 @@
 import { useNavigate } from "react-router-dom";
 import CartItem from "../cart/CartItem";
+import { useState } from "react";
+import EmptyCart from "../cart/EmptyCart";
 
 function Cart() {
   const navigate = useNavigate();
+  const [isEmpty, setIsEmpty] = useState(false);
 
   return (
     <div className="w-full flex flex-col">
-      <ul className="h-[60%] rounded-lg mt-13 p-2 laptop:p-3 desktop:p-10 laptop:h-[65vh] laptop:overflow-y-scroll">
-        <CartItem />
-        <CartItem />
-        <CartItem />
-        <CartItem />
-        <CartItem />
-        <CartItem />
-        <CartItem />
-        <CartItem />
-        <CartItem />
-        <CartItem />
-        <CartItem />
-        <CartItem />
-        <CartItem />
-      </ul>
-      <div className="flex flex-col p-4 text-zinc-700 laptop:text-xl laptop:p-6 laptop:mt-1">
-        <div className="flex justify-between laptop:pb-2">
-          <p>Subtotal:</p>
-          <p>$146</p>
-        </div>
-        <div className="flex justify-between border-zinc-800 border-b-2 pb-2.5 laptop:pb-3">
-          <p>Delivery:</p>
-          <p>$15</p>
-        </div>
-        <div className="flex justify-between text-zinc-900 font-semibold mt-2 laptop:text-2xl">
-          <p>Total</p>
-          <p>$161</p>
-        </div>
-      </div>
-      <button
-        className="bg-zinc-800 w-24 h-8 rounded-md text-zinc-100 mx-auto mb-4 mt-2 laptop:cursor-pointer laptop:w-40 laptop:h-12 laptop:rounded-xl laptop:text-2xl hover:bg-zinc-700 transition-all duration-200"
-        onClick={() => navigate("/order")}
-      >
-        Checkout
-      </button>
+      {isEmpty ? (
+        <EmptyCart />
+      ) : (
+        <>
+          {" "}
+          <ul className="h-[60%] rounded-lg mt-13 p-2 laptop:p-3 desktop:p-10 laptop:h-[65vh] laptop:overflow-y-scroll">
+            <CartItem />
+            <CartItem />
+            <CartItem />
+            <CartItem />
+          </ul>
+          <div className="flex flex-col p-4 text-zinc-700 laptop:text-xl laptop:p-6 laptop:mt-1">
+            <div className="flex justify-between laptop:pb-2">
+              <p>Subtotal:</p>
+              <p>$146</p>
+            </div>
+            <div className="flex justify-between border-zinc-800 border-b-2 pb-2.5 laptop:pb-3">
+              <p>Delivery:</p>
+              <p>$15</p>
+            </div>
+            <div className="flex justify-between text-zinc-900 font-semibold mt-2 laptop:text-2xl">
+              <p>Total</p>
+              <p>$161</p>
+            </div>
+          </div>
+          <button
+            className="bg-zinc-800 w-24 h-8 rounded-md text-zinc-100 mx-auto mb-4 mt-2 laptop:cursor-pointer laptop:w-40 laptop:h-12 laptop:rounded-xl laptop:text-2xl hover:bg-zinc-700 transition-all duration-200"
+            onClick={() => navigate("/order")}
+          >
+            Checkout
+          </button>
+        </>
+      )}
     </div>
   );
 }
