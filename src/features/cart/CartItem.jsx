6e0f7bc41506df49a85c6e444fb84b6ddc243053
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-06-03 11:46:52 +0330

    cart responsive

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,17 +1,30 @@
 function CartItem() {
   return (
-    <li className="w-full bg-zinc-50 flex justify-between items-center rounded-lg shadow p-2 mb-2">
-      <div className="flex items-center gap-4">
-        <img src="" alt="product" className="h-full w-10" />
+    <li className="w-full bg-zinc-50 flex justify-between items-center rounded-lg shadow p-2 mb-2 laptop:h-24 laptop:mb-3 laptop:px-4">
+      <div className="flex items-center gap-8">
+        <img
+          src=""
+          alt="product"
+          className="w-10 h-10 rounded-lg laptop:h-20 laptop:w-20"
+        />
         <div>
-          <p className="text-lg font-medium tracking-wide">title</p>
-          <p className="tracking-wide">$21</p>
+          <p className="text-lg font-medium tracking-wide laptop:font-semibold laptop:text-xl">
+            title
+          </p>
+          <p className="tracking-wide text-lg">$21</p>
         </div>
       </div>
-      <div className="bg-zinc-700 rounded-md w-12 text-zinc-100 px-1.5 flex items-center justify-between">
-        <button>-</button>
-        <span>1</span>
-        <button>+</button>
+      <div className="flex gap-2">
+        <div className="bg-zinc-700 rounded-md w-12 text-zinc-100 px-1.5 flex items-center justify-between laptop:text-xl laptop:w-16 font-semibold">
+          <button className="laptop:cursor-pointer">-</button>
+          <span>10</span>
+          <button className="laptop:cursor-pointer">+</button>
+        </div>
+        <img
+          src="./trash.svg"
+          alt="trash"
+          className="w-6 hidden laptop:block"
+        />
       </div>
     </li>
   );
--- src/features/pages/Cart.jsx
+++ src/features/pages/Cart.jsx
@@ -3,7 +3,7 @@ import CartItem from "../cart/CartItem";
 function Cart() {
   return (
     <div className="w-full flex flex-col">
-      <ul className="h-[60%] rounded-lg mt-13 p-2">
+      <ul className="h-[60%] rounded-lg mt-13 p-2 laptop:p-3 desktop:p-10 laptop:h-[65vh] laptop:overflow-y-scroll">
         <CartItem />
         <CartItem />
         <CartItem />
@@ -18,21 +18,21 @@ function Cart() {
         <CartItem />
         <CartItem />
       </ul>
-      <div className="flex flex-col p-4 text-zinc-700">
-        <div className="flex justify-between">
+      <div className="flex flex-col p-4 text-zinc-700 laptop:text-xl laptop:p-6 laptop:mt-1">
+        <div className="flex justify-between laptop:pb-2">
           <p>Subtotal:</p>
           <p>$146</p>
         </div>
-        <div className="flex justify-between border-zinc-800 border-b-2 pb-2.5">
+        <div className="flex justify-between border-zinc-800 border-b-2 pb-2.5 laptop:pb-3">
           <p>Delivery:</p>
           <p>$15</p>
         </div>
-        <div className="flex justify-between text-zinc-900 font-semibold mt-2">
+        <div className="flex justify-between text-zinc-900 font-semibold mt-2 laptop:text-2xl">
           <p>Total</p>
           <p>$161</p>
         </div>
       </div>
-      <button className="bg-zinc-800 w-24 h-8 rounded-md text-zinc-100 mx-auto mb-4 mt-2 laptop:cursor-pointer">
+      <button className="bg-zinc-800 w-24 h-8 rounded-md text-zinc-100 mx-auto mb-4 mt-2 laptop:cursor-pointer laptop:w-40 laptop:h-12 laptop:rounded-xl laptop:text-2xl hover:bg-zinc-700 transition-all duration-200">
         Checkout
       </button>
     </div>
