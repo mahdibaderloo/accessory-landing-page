c697b2943019b251ce90620cd74f12ea0bc72745
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-06-10 21:04:03 +0330

    add search box to products page

--- src/features/pages/Products.jsx
+++ src/features/pages/Products.jsx
@@ -1,9 +1,11 @@
+import Search from "../../components/Search";
 import Categories from "../products/Categories";
 
 function Products() {
   return (
     <div className="w-full h-[90vh] mt-16">
       <Categories />
+      <Search />
     </div>
   );
 }
