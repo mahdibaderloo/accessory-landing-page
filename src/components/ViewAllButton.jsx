a197051a17f757659291f9c501264b3a7db325a5
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-06-13 08:29:21 +0330

    view all component responsive

--- src/components/ViewAllButton.jsx
+++ src/components/ViewAllButton.jsx
@@ -5,7 +5,7 @@ function ViewAllButton() {
   return (
     <Link
       to="/products"
-      className="bg-transparent flex gap-2 w-fit mx-auto justify-center items-center border-2 border-zinc-600 rounded-2xl px-4 py-1 m-8 laptop:cursor-pointer laptop:hover:bg-zinc-100 transition-all duration-200"
+      className="bg-transparent flex gap-2 w-fit mx-auto justify-center items-center border-2 border-zinc-600 rounded-2xl text-sm px-1.5 py-0.5 tablet:px-4 tablet:py-1 laptop:text-lg m-8 laptop:cursor-pointer laptop:hover:bg-zinc-100 transition-all duration-200"
     >
       <img src={bagIcon} alt="icon" className="w-4" />
       <span className="text-shadow-zinc-800">View All</span>
