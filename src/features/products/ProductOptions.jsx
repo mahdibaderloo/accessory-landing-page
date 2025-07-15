0dacd6037c8af633157e9d54358ca656144f225b
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-07-15 20:34:32 +0330

    add favorite button styles

--- src/features/products/ProductOptions.jsx
+++ src/features/products/ProductOptions.jsx
@@ -4,14 +4,17 @@ import FillHeartIcon from "../../data/images/heart-fill.svg";
 import { useDispatch, useSelector } from "react-redux";
 import { chooseColor, chooseSize } from "./productSlice";
 import { addItem } from "../cart/cartSlice";
+import { useState } from "react";
 
 function ProductOptions({ product }) {
-  const { id, name, price, description } = product;
+  const [isFavorite, setIsFavorite] = useState(false);
 
   const size = useSelector((state) => state.product.size);
   const color = useSelector((state) => state.product.color);
 
   const dispatch = useDispatch();
+
+  const { id, name, price, description } = product;
 
   const sizes = new Array(11).fill(30).map((n, i) => n + i);
   const colors = ["Black", "White", "Red", "Yellow", "Blue", "Purple", "Gray"];
@@ -33,12 +36,21 @@ function ProductOptions({ product }) {
     dispatch(addItem(newItem));
   }
 
+  function handleAddToFavorites() {}
+
   return (
     <form className="w-full p-2 tablet:w-[70%] tablet:mx-auto laptop:mx-0 laptop:p-0 laptop:mt-4 laptop:w-full">
-      <div className="flex items-center gap-1 mb-4 bg-zinc-200 w-fit px-2 py-1.5 laptop:px-3 laptop:cursor-pointer">
-        <img src={EmptyHeartIcon} alt="" className="w-5" />
-        <p className=" text-zinc-800 text-sm tablet:text-[24px] laptop:text-lg">
-          Add to favorites
+      <div className="flex items-center gap-1 mb-4 bg-zinc-200 w-fit px-2 py-1.5 laptop:px-3 laptop:cursor-pointer hover:bg-zinc-300 transition-all duration-200">
+        <img
+          src={isFavorite ? FillHeartIcon : EmptyHeartIcon}
+          alt=""
+          className="w-5"
+        />
+        <p
+          className=" text-zinc-800 text-sm tablet:text-[24px] laptop:text-lg"
+          onClick={handleAddToFavorites}
+        >
+          {isFavorite ? "Remove from favorites" : "Add to favorites"}
         </p>
       </div>
       <div className="w-full flex gap-2">
