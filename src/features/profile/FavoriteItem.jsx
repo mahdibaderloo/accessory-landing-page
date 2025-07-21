f560d808ccef63f9e1a025a26257fab94edfa055
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-07-21 11:05:03 +0330

    show favorite items content

--- src/features/profile/FavoriteItem.jsx
+++ src/features/profile/FavoriteItem.jsx
@@ -1,4 +1,6 @@
-function FavoriteItem({ product }) {
+function FavoriteItem({ item }) {
+  const { name, price } = item;
+
   return (
     <li className="bg-zinc-50 flex flex-col w-26 h-fit tablet:w-32 laptop:w-36 desktop:w-48 gap-4 laptop:gap-4 desktop:gap-4 shadow p-2 rounded-xl tablet:p-3 laptop:cursor-pointer">
       <img
@@ -7,8 +9,8 @@ function FavoriteItem({ product }) {
         className="w-22 h-22 self-center rounded-sm  laptop:rounded-lg desktop:rounded-xl tablet:w-28 tablet:h-28 laptop:w-32 laptop:h-32 desktop:w-42 desktop:h-42"
       />
       <div className="text-sm w-full tablet:text-md laptop:text-lg desktop:text-xl">
-        <p className="font-medium break-all">product title</p>
-        <p className="">$245</p>
+        <p className="font-medium break-all">{name}</p>
+        <p className="">${price}</p>
       </div>
     </li>
   );
