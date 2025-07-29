b5f99498ee49f3fb509f5c6b75418ef24d4f57e9
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-07-29 15:43:25 +0330

    use my information in checkout

--- src/features/pages/Checkout.jsx
+++ src/features/pages/Checkout.jsx
@@ -1,13 +1,39 @@
 import { useSelector } from "react-redux";
 
 import checkIcon from "../../data/images/check.svg";
+import { useEffect, useState } from "react";
 
 function Checkout() {
+  const [name, setName] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [address, setAddress] = useState("");
+  const [isCheck, setIsCheck] = useState(false);
+
   const user = useSelector((state) => state.profile.user);
+
+  useEffect(
+    function () {
+      if (isCheck) myInformation();
+      else otherInformation();
+    },
+    [isCheck]
+  );
 
   if (!user) return <Loader />;
 
   const { name: nameApi, mobile: mobileApi, address: addressApi } = user[0];
+
+  function myInformation() {
+    setName(nameApi);
+    setMobile(mobileApi);
+    setAddress(addressApi);
+  }
+
+  function otherInformation() {
+    setName("");
+    setMobile("");
+    setAddress("");
+  }
 
   return (
     <form className="mt-16 p-4 border border-zinc-300 shadow rounded-xl">
@@ -17,7 +43,7 @@ function Checkout() {
           Please complete the information below
         </p>
       </div>
-      <div className="py-3 flex flex-col laptop:flex-row justify-between border-b border-b-zinc-200">
+      <div className="py-3 flex items-center flex-col laptop:flex-row justify-between border-b border-b-zinc-200">
         <label htmlFor="username" className="font-medium text-zinc-800">
           Name
         </label>
@@ -25,11 +51,12 @@ function Checkout() {
           type="text"
           id="username"
           placeholder="your name"
-          defaultValue={nameApi}
-          className="outline-none border-none w-1/2 bg-zinc-50 p-1 rounded-lg"
+          defaultValue={name}
+          onChange={(e) => setName(e.target.value)}
+          className="outline-none border-none w-1/2 bg-zinc-50 p-2 rounded-lg"
         />
       </div>
-      <div className="py-3 flex flex-col laptop:flex-row justify-between border-b border-b-zinc-200">
+      <div className="py-3 flex items-center flex-col laptop:flex-row justify-between border-b border-b-zinc-200">
         <label htmlFor="mobile" className="font-medium text-zinc-800">
           Mobile number
         </label>
@@ -37,8 +64,9 @@ function Checkout() {
           type="text"
           id="mobile"
           placeholder="Add number"
-          defaultValue={`+98 ${mobileApi || ""}`}
-          className="outline-none border-none w-1/2 bg-zinc-50 p-1 rounded-lg"
+          defaultValue={`+98 ${mobile || ""}`}
+          onChange={(e) => setMobile(e.target.value)}
+          className="outline-none border-none w-1/2 bg-zinc-50 p-2 rounded-lg"
         />
       </div>
       <div className="py-2 flex flex-col laptop:flex-row justify-between">
@@ -50,9 +78,21 @@ function Checkout() {
           id="address"
           placeholder="Iran..."
           dir="ltr"
-          defaultValue={addressApi}
+          defaultValue={address}
+          onChange={(e) => setAddress(e.target.value)}
           className="outline-none border-none w-1/2 bg-zinc-50 p-1 rounded-lg"
         ></textarea>
+      </div>
+      <div className="flex items-center gap-1 text-zinc-800 hover:text-zinc-900 transition-all duration-200 cursor-pointer">
+        <input
+          type="checkbox"
+          id="check"
+          className="cursor-pointer scale-150 accent-zinc-800"
+          onChange={() => setIsCheck((check) => !check)}
+        />
+        <label htmlFor="check" className="cursor-pointer font-medium">
+          Use my information
+        </label>
       </div>
       <div className="py-2 flex flex-col gap-4 laptop:flex-row justify-end">
         <button className="bg-emerald-600 text-white px-3 py-1 rounded-sm hover:bg-emerald-700 transition-all duration-200 cursor-pointer">
