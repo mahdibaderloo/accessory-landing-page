37df055a2b7f08f32611de91950f8804a91528d4
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-07-29 15:21:01 +0330

    checkout styles

--- src/features/pages/Checkout.jsx
+++ src/features/pages/Checkout.jsx
@@ -1,5 +1,7 @@
 import { useSelector } from "react-redux";
 
+import checkIcon from "../../data/images/check.svg";
+
 function Checkout() {
   const user = useSelector((state) => state.profile.user);
 
@@ -9,6 +11,12 @@ function Checkout() {
 
   return (
     <form className="mt-16 p-4 border border-zinc-300 shadow rounded-xl">
+      <div className="flex items-center gap-2 border-b border-b-zinc-500 w-fit pb-2 mb-8">
+        <img src={checkIcon} alt="" className="w-7" />
+        <p className="font-semibold text-xl text-zinc-800">
+          Please complete the information below
+        </p>
+      </div>
       <div className="py-3 flex flex-col laptop:flex-row justify-between border-b border-b-zinc-200">
         <label htmlFor="username" className="font-medium text-zinc-800">
           Name
@@ -18,7 +26,7 @@ function Checkout() {
           id="username"
           placeholder="your name"
           defaultValue={nameApi}
-          className="outline-none border-none w-1/2"
+          className="outline-none border-none w-1/2 bg-zinc-50 p-1 rounded-lg"
         />
       </div>
       <div className="py-3 flex flex-col laptop:flex-row justify-between border-b border-b-zinc-200">
@@ -30,7 +38,7 @@ function Checkout() {
           id="mobile"
           placeholder="Add number"
           defaultValue={`+98 ${mobileApi || ""}`}
-          className="outline-none border-none w-1/2"
+          className="outline-none border-none w-1/2 bg-zinc-50 p-1 rounded-lg"
         />
       </div>
       <div className="py-2 flex flex-col laptop:flex-row justify-between">
@@ -43,10 +51,10 @@ function Checkout() {
           placeholder="Iran..."
           dir="ltr"
           defaultValue={addressApi}
-          className="outline-none border-none w-1/2"
+          className="outline-none border-none w-1/2 bg-zinc-50 p-1 rounded-lg"
         ></textarea>
       </div>
-      <div className="py-2 flex flex-col gap-6 laptop:flex-row justify-center">
+      <div className="py-2 flex flex-col gap-4 laptop:flex-row justify-end">
         <button className="bg-emerald-600 text-white px-3 py-1 rounded-sm hover:bg-emerald-700 transition-all duration-200 cursor-pointer">
           Save and pay
         </button>
