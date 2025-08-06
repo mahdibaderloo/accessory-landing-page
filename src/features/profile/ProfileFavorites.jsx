5702264f5db3d0fd0d7b66a7851a0aef380044fd
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-08-06 20:13:03 +0330

    isEmpty for favorites

--- src/features/profile/ProfileFavorites.jsx
+++ src/features/profile/ProfileFavorites.jsx
@@ -4,17 +4,24 @@ import trashIcon from "../../data/images/trash-red.svg";
 import FavoriteItem from "./FavoriteItem";
 import useAuth from "../../components/useAuth";
 import { clearFavorites } from "./profileSlice";
+import Empty from "./Empty";
+import Loader from "../../components/Loader";
 
 function ProfileFavorites() {
   useAuth();
   const dispatch = useDispatch();
   const favorites = useSelector((state) => state.profile.favorites);
   const user = useSelector((state) => state.profile.user);
+  const status = useSelector((state) => state.profile.status);
 
   function handleRemoveAll() {
     const userId = user?.[0]?.id;
     dispatch(clearFavorites(userId));
   }
+
+  if (status === "loading") return <Loader />;
+
+  if (favorites.length === 0) return <Empty>Favorites list is empty!</Empty>;
 
   return (
     <div className="w-full h-screen laptop:w-[70%] p-2 laptop:p-0 laptop:m-8 overflow-hidden">
