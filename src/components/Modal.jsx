853e106f7f5adfc1e43d098c825c6b17e5658b8c
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-08-15 12:07:39 +0330

    modal responsive

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -12,19 +12,19 @@ function Modal({ children, onConfirm }) {
       <div className="absolute left-0 top-0 w-full h-screen font-barlow z-50">
         <div className="blur-md bg-zinc-200 opacity-90 w-full h-screen"></div>
         <div className="w-full h-screen absolute top-0 left-0 flex justify-center items-center">
-          <div className="shadow-lg bg-zinc-100 w-[50%] p-4 flex flex-col rounded-2xl">
+          <div className="shadow-lg bg-zinc-100 w-[95%] laptop:w-[50%] p-4 flex flex-col rounded-2xl">
             <img
               src={closeIcon}
               alt="icon"
-              className="w-6 cursor-pointer"
+              className="w-4 laptop:w-6 cursor-pointer"
               onClick={() => dispatch(closeModal())}
             />
-            <p className="text-center font-medium text-xl text-zinc-800 mt-2">
+            <p className="text-center font-medium text-sm laptop:text-xl text-zinc-800 mt-2">
               {children}
             </p>
             <div className="flex justify-center gap-3 mt-4">
               <button
-                className="bg-emerald-600 hover:bg-emerald-700 transition-all duration-200 shadow text-zinc-50 px-3 py-1 rounded-md cursor-pointer"
+                className="bg-emerald-600 hover:bg-emerald-700 transition-all duration-200 text-sm laptop:text-lg shadow text-zinc-50 px-3 py-1 rounded-md cursor-pointer"
                 onClick={() => {
                   if (onConfirm) onConfirm();
                   dispatch(closeModal());
@@ -33,7 +33,7 @@ function Modal({ children, onConfirm }) {
                 Yes
               </button>
               <button
-                className="bg-red-600 hover:bg-red-700 transition-all duration-200 shadow text-zinc-50 px-3 py-1 rounded-md cursor-pointer"
+                className="bg-red-600 hover:bg-red-700 transition-all duration-200 text-sm laptop:text-lg shadow text-zinc-50 px-3 py-1 rounded-md cursor-pointer"
                 onClick={() => dispatch(closeModal())}
               >
                 No
--- src/features/profile/ProfileFavorites.jsx
+++ src/features/profile/ProfileFavorites.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import editIcon from "../../data/images/edit.svg";
+// import editIcon from "../../data/images/edit.svg";
 import trashIcon from "../../data/images/trash-red.svg";
 import FavoriteItem from "./FavoriteItem";
 import useAuth from "../../components/useAuth";
@@ -29,11 +29,11 @@ function ProfileFavorites() {
     <div className="w-full h-screen laptop:w-[70%] p-2 laptop:p-0 laptop:m-8 overflow-hidden">
       <div>
         <div className="flex justify-end items-center gap-2">
-          <img
+          {/* <img
             src={editIcon}
             alt="icon"
             className="w-3.5 laptop:w-5.5 laptop:cursor-pointer"
-          />
+          /> */}
           <img
             src={trashIcon}
             alt="icon"
