5b2f30bb3cd72d2cf520c9077e27f2ded3610c62
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-08-15 10:42:05 +0330

    empty styles

--- src/features/profile/Empty.jsx
+++ src/features/profile/Empty.jsx
@@ -1,7 +1,9 @@
 function Empty({ children }) {
   return (
     <div className="w-full h-full flex flex-col justify-center items-center gap-6 laptop:w-[70%] p-4 laptop:p-0 laptop:mx-8">
-      <p className="text-4xl font-semibold text-zinc-800">{children}</p>
+      <p className="text-md laptop:text-4xl font-semibold text-zinc-800">
+        {children}
+      </p>
     </div>
   );
 }
