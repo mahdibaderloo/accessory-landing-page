e173587601c3228344e5fa2b95e42da1918d64f6
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-08-15 11:05:34 +0330

    cart responsive

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -6,7 +6,6 @@ function CartItem({ item }) {
   const dispatch = useDispatch();
 
   const { id, name, price, image, count } = item;
-  console.log(image);
 
   function handleDecrease() {
     dispatch(decreaseItemCount(id));
@@ -21,22 +20,22 @@ function CartItem({ item }) {
   }
 
   return (
-    <li className="w-full bg-zinc-50 flex justify-between items-center rounded-lg shadow p-2 mb-2 laptop:h-24 laptop:mb-3 laptop:px-4">
-      <div className="flex items-center gap-8">
+    <li className="w-full bg-zinc-50 flex flex-col laptop:flex-row justify-between items-center rounded-lg shadow p-2 mb-2 laptop:h-24 laptop:mb-3 laptop:px-4">
+      <div className="w-full flex justify-between laptop:justify-start items-center gap-8">
         <img
           src={image}
           alt="product"
-          className="w-10 h-10 rounded-lg laptop:h-20 laptop:w-20"
+          className="w-14 h-14 rounded-lg laptop:h-20 laptop:w-20"
         />
         <div>
-          <p className="text-lg font-medium tracking-wide laptop:font-semibold laptop:text-xl">
+          <p className="text-sm font-medium tracking-wide laptop:font-semibold laptop:text-xl">
             {name}
           </p>
-          <p className="tracking-wide text-lg">${price}</p>
+          <p className="hidden tracking-wide text-lg laptop:block">${price}</p>
         </div>
       </div>
-      <div className="flex gap-2">
-        <div className="bg-zinc-700 rounded-md w-12 text-zinc-100 px-1.5 flex items-center justify-between laptop:text-xl laptop:w-16 font-semibold">
+      <div className="w-full flex justify-between tablet:flex-row laptop:justify-end gap-2 mt-2">
+        <div className="bg-zinc-700 rounded-md w-14 text-zinc-100 px-1.5 flex items-center justify-between laptop:text-xl laptop:w-16 font-semibold">
           <button onClick={handleDecrease} className="laptop:cursor-pointer">
             -
           </button>
@@ -45,6 +44,7 @@ function CartItem({ item }) {
             +
           </button>
         </div>
+        <p className="tracking-wide text-lg laptop:hidden">${price}</p>
         <img
           src={trashIcon}
           onClick={handleRemove}
