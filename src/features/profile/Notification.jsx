1a55f041927a55e2342eb01fcdefcddbf21b05ee
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-08-22 17:18:44 +0330

    refactor profile notification page

--- src/features/profile/Notification.jsx
+++ src/features/profile/Notification.jsx
@@ -14,14 +14,14 @@ function Notification() {
     <div className="w-full laptop:w-[70%] p-4 laptop:p-0 laptop:m-8">
       <Link
         to={-1}
-        className="text-center leading-[30px] rounded-md font-barlow bg-zinc-500 hover:bg-zinc-800 text-zinc-100 transition-all duration-200 laptop:rounded-xl py-1 px-2 laptop:py-2 laptop:px-3 laptop:leading-[40px] desktop:mr-87"
+        className="text-center rounded-md font-barlow bg-zinc-500 hover:bg-zinc-800 text-zinc-100 transition-all duration-200 laptop:rounded-xl py-2 px-3 laptop:py-2 laptop:leading-[40px] desktop:mr-87"
       >
         &larr; Back
       </Link>
       <div className="flex flex-col gap-6 mt-8 bg-zinc-50 h-fit p-4 rounded-lg laptop:rounded-2xl shadow">
         <h3 className="text-3xl font-medium text-zinc-800">{title}</h3>
         <div
-          className="text-zinc-700 font-medium mb-8 flex"
+          className="text-zinc-700 text-lg font-medium mb-8 flex"
           dangerouslySetInnerHTML={{
             __html: description,
           }}
